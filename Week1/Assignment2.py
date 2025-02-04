40861bdc81bd23af2589bd2d86024b470524e7b2
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2025-02-04 21:39:32 +0600

    Create Assignment2.py

---
+++ Week1/Assignment2.py
@@ -0,0 +1,12 @@
+# Sum all the odd numbers from 0 to 100 and print it to the screen.
+# Code:
+
+
+sum  = 0
+i = 0
+while i>=0 and i<100:
+    if i%2 != 0:
+        sum = sum + i
+    i = i + 1
+	
+print(sum)
