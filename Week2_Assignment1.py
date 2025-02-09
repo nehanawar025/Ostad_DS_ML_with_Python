ca7ff0ca74559dec199c1beb527d6e060691c0d6
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2025-02-10 04:24:19 +0600

    Add files via upload

---
+++ Week2_Assignment1.py
@@ -0,0 +1,16 @@
+count_dict = {}
+
+num_list = [1, 5, 6, 5, 1, 2, 3]
+dup_list = []
+
+for i in num_list:
+    if i not in count_dict:
+        count_dict[i] = 1
+    else:
+        count_dict[i] += 1
+
+for key, value in count_dict.items():
+    if value > 1:
+        dup_list.append(key)
+
+print(dup_list)
