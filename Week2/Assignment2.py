3a1cf3a7ce8ebb11f0385c58d9d72dd3d8fba913
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2025-02-12 21:04:55 +0600

    Add files via upload

---
+++ Week2/Assignment2.py
@@ -0,0 +1,15 @@
+# Write a program to sort by ascending order from the following list. (Create a function to solve it)
+
+def ascending_sort(list):
+    n = len(list)
+    
+    for i in range(n-1): 
+        for j in range(n-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+                
+    print(list)
+
+
+list = [1, 5, 2, 9, 3, 22, 13]
+ascending_sort(list)
